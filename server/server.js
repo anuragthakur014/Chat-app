1fc6efb3ecab7f4629ba9cff40b2eb4707a8dc80
anuragthakur014 <developer@krishnasoftware.com>
2026-05-27 17:08:39 +0530

    fix render port

--- server/server.js
+++ server/server.js
@@ -295,6 +295,10 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(process.env.PORT || 5000, () => {
+
+  console.log(
+    `Server running on port ${process.env.PORT || 5000}`
+  );
+
 });
