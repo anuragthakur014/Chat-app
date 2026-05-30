b713f142edf52e16a517f69a4d54cb07d0b20fbc
anuragthakur014 <developer@krishnasoftware.com>
2026-05-30 11:03:00 +0530

    fixed socket polling

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,8 +3,14 @@ import { io } from "socket.io-client";
 const socket = io(
   "https://chat-app-gtzp.onrender.com",
   {
-    transports: ["websocket", "polling"],
+    transports: ["polling"],
+
+    reconnection: true,
+
+    reconnectionAttempts: 10,
+
+    reconnectionDelay: 1000,
   }
 );
 
-export default socket;  
+export default socket;
